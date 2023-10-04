a260654ecabac3d7ee69e447e82841e18c9e2aef
tiwinur <tiwinurhalimah0801@gmail.com>
2023-10-04 17:39:50 +0700

    cooding challenge 3-8okt

---
+++ pangkattiga.py
@@ -0,0 +1,7 @@
+# Bilangan tertentu yang diberikan
+angka_masukan = 6
+
+# Loop untuk menghitung pangkat tiga dari bilangan dari 1 hingga angka_masukan
+for i in range(1, angka_masukan + 1):
+    kubus = i ** 3
+    print(f"Bilangan sekarang: {i} dan kubus {kubus}")
